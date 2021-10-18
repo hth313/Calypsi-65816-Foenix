11f3b7a85ee72261d49d17ff5c45b37016a6080f
Håkan Thörngren <hth313@gmail.com>
2021-10-18 12:20:14 -0700

    Define run-time attributes for assembler files
    Actually set the code and data model run-time attribute in the output
    file for extra security.

--- src/macros.h
+++ src/macros.h
@@ -37,4 +37,34 @@ jump          .macro  dest
 
 #endif // __CALYPSI_CODE_MODEL_SMALL__
 #endif // __CALYPSI_ASSEMBLER__
+
+
+// ----------------------------------------------------------------------
+//
+// Define code and data model used. This is to add a bit of safety in
+// case the way a file is assembled is combined with the wrong run-time.
+//
+// ----------------------------------------------------------------------
+
+#if defined(__CALYPSI_DATA_MODEL_SMALL__)
+              .rtmodel dataModel,"small"
+#elif defined (__CALYPSI_DATA_MODEL_MEDIUM__)
+              .rtmodel dataModel,"medium"
+#elif defined(__CALYPSI_DATA_MODEL_LARGE__)
+              .rtmodel dataModel,"large"
+#elif defined(__CALYPSI_DATA_MODEL_HUGE__)
+              .rtmodel dataModel,"huge"
+#else
+#pragma GCC error "unexpected data model"
+#endif
+
+#if defined(__CALYPSI_CODE_MODEL_SMALL__)
+              .rtmodel codeModel,"small"
+#elif defined(__CALYPSI_CODE_MODEL_LARGE__)
+              .rtmodel codeModel,"large"
+#else
+#pragma GCC error "unexpected code model"
+#endif
+
+
 #endif // __MACROS_H
