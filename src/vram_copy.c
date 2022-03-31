7035bf53ab0764a473af7084e07a7230c43c980b
Håkan Thörngren <hth313@gmail.com>
2022-03-31 10:19:41 -0700

    Correct SDMA to VDMA transfer
    Use the correct VRAM style destination address. As this is an inline
    function we must force inlining to be used.
    
    Also correct the control bits setup so that the system understand that
    we are doing an SDMA to VDMA transfer.

--- src/vram_copy.c
+++ src/vram_copy.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "foenix/dma.h"
+#include "foenix/vicky.h"
 #include "support.h"
 
 #if defined(__CALYPSI_TARGET_65816__) && defined(__CALYPSI_DATA_MODEL_SMALL__)
@@ -12,8 +13,12 @@ void *memcpy (void *dest, const void *src, size_t n) {
 #endif
   if (in_vram(dest)) {
 
+    // Enable DMA and VDMA
+    VDMA_CONTROL_REG = VDMA_CTRL_Enable | VDMA_CTRL_SysRAM_Src;
+    SDMA_CTRL_REG0 = SDMA_CTRL0_Enable | SDMA_XFER_SRAM2VRAM;
+
     SDMA_SRC_ADDY = src;
-    VDMA_DST_ADDY = dest;
+    VDMA_DST_ADDY = vicky_address(dest);
 
     SDMA_SIZE = n;
     VDMA_SIZE = n;
@@ -30,6 +35,7 @@ void *memcpy (void *dest, const void *src, size_t n) {
     DMA_wait_delay();
 
     SDMA_CTRL_REG0 = 0;
+
     wait_for_DMA_to_finish();
 
   } else {
