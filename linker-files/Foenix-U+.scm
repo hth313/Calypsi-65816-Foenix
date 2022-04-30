2ca5e0d73e585cf0f2e5e472102f9e5aaa057e38
Håkan Thörngren <hth313@gmail.com>
2022-04-29 20:25:58 -0700

    Update linker files
    Remove unused call to 'append' and correct the U variant which had
    mismatched parentheses. Also re-indent them after removing 'append'.

--- linker-files/Foenix-U+.scm
+++ linker-files/Foenix-U+.scm
@@ -1,14 +1,13 @@
 ;;; Example linker rules for the for the C256 Foenix U+
 
 (define memories
-  (append
-   '((memory Banks (address (#x10000 . #x2fffff)) (type ANY))
-     (memory LoMem (address (#x2100 . #x7fff)) (type ANY))
-     (memory Vector (address (#xfff0 . #xffff)))
+  '((memory Banks (address (#x10000 . #x2fffff)) (type ANY))
+    (memory LoMem (address (#x2100 . #x7fff)) (type ANY))
+    (memory Vector (address (#xfff0 . #xffff)))
 
-     (memory palettes (address (#xaf2000 . #xaf3fff))
-             (section palette0 palette1 palette2 palette3
-                      palette4 palette5 palette6 palette7))
-     (memory vram (address (#xb00000 . #xcfffff))
-             (section vram))
-     )))
+    (memory palettes (address (#xaf2000 . #xaf3fff))
+            (section palette0 palette1 palette2 palette3
+                     palette4 palette5 palette6 palette7))
+    (memory vram (address (#xb00000 . #xcfffff))
+            (section vram))
+    ))
