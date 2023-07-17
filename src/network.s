3affe8d6e8d90e2d27e0e05c0d1a3074ac710174
Håkan Thörngren <hth313@gmail.com>
2023-07-16 21:27:18 -0700

    Correct network interface code
    Various fixes related to code and data models.

--- src/network.s
+++ src/network.s
@@ -6,20 +6,40 @@ UDP_RECV:     .equlab 0x1158
 
 	      .section libcode
 	      .public ip_init, udp_send, udp_recv
+	      .extern _Dp
 
-ip_init:      txy
+ip_init:      phb
+	      ldy     dp:.tiny(_Dp+0) ; B:Y = 24-bit pointer
+	      pei     dp:.tiny(_Dp+1)
+	      plb
+	      plb
 	      jsl     IP_INIT
+	      plb
 carry_to_value:
 	      lda     ##0
 	      bcc     10$
 	      dec     a
-10$: 	      rts
+10$: 	      return
 
-udp_send:     jsl     UDP_SEND
+udp_send:
+#ifdef __CALYPSI_DATA_MODEL_SMALL__
+	      tax
+#else
+	      ldx     dp:.tiny(_Dp+0)
+#endif
+	      lda     ##0
+	      jsl     UDP_SEND
 	      bra     carry_to_value
 
-udp_recv:     jsl     UDP_RECV
+udp_recv:
+#ifdef __CALYPSI_DATA_MODEL_SMALL__
+	      tax
+#else
+	      ldx     dp:.tiny(_Dp+0)
+#endif
+	      lda     ##0
+	      jsl     UDP_RECV
 	      bcs     carry_to_value
 	      beq     carry_to_value
 	      lda     ##1
-	      rts
+	      return
